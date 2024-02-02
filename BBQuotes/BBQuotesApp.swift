8eff674ec2058478cc5048ad2fd567e9ac1e852c
joaobug <joaoenfrance@gmail.com>
2024-02-02 14:04:20 -0300

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Joao Leal on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
